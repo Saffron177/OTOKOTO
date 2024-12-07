50467c5dd4ddb31f58a4a88145eed24e8d15efd0
Shuhei Yamada <marine-grey_007--nana_riri@docomo.ne.jp>
2024-12-07 19:18:14 +0900

    フィラー除去のexeファイルを追加

---
+++ Filler_removal.py
@@ -0,0 +1,26 @@
+﻿import spacy
+import sys
+import spacy.util
+import os
+# コマンドライン引数から transcribed_text を受け取る
+if len(sys.argv) > 1:
+    transcribed_text = sys.argv[1]
+else:
+    transcribed_text = ""
+
+
+nlp = spacy.load('ja_ginza')
+
+# モデルのパスを取得
+#model_package_path = spacy.util.get_package_path("ja_ginza")
+#print(f"ロードされたモデルのパッケージパス: {model_package_path}")
+
+
+doc = nlp(transcribed_text)
+# フィラーの削除
+result = ''
+for sent in doc.sents:
+  for token in sent:
+    if token.tag_ != "感動詞-フィラー":
+      result += str(token.text)
+print(result)
