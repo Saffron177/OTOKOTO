2db7aea355adde1af7e602fac370a9fcaff118cf
Shuhei Yamada <marine-grey_007--nana_riri@docomo.ne.jp>
2024-12-10 10:40:39 +0900

    フィラー除去クラスの追加

---
+++ Filler_Removal.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.VisualBasic.ApplicationServices;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HottoMotto
+{
+    public class Filler_Removal
+    {
+        /// <summary>
+        /// フィラーを除去するexeを実行する関数（非同期）
+        /// </summary>
+        /// <param name="arg"></param>
+        /// <returns></returns>
+        public async Task<string> removal(string arg)
+        {
+            try
+            {
+                // プロセスの設定
+                var processStartInfo = new ProcessStartInfo
+                {
+                    FileName = "Filler_removal.exe",    //実行するexe
+                    Arguments = arg,                    //引数
+                    RedirectStandardOutput = true, // 標準出力をリダイレクト
+                    RedirectStandardError = true, // 標準エラー出力をリダイレクト
+                    UseShellExecute = false,     // シェルを使用しない
+                    CreateNoWindow = true        // ウィンドウを作成しない
+                };
+
+                using (var process = new Process { StartInfo = processStartInfo })
+                {
+                    var tcs = new TaskCompletionSource<bool>();
+
+                    //process.Exited += (sender, e) =>
+                    //{
+                    //    tcs.SetResult(true);
+                    //    process.Dispose();
+                    //};
+
+                    process.Start();
+                    // 標準出力を取得
+                    string output = await process.StandardOutput.ReadToEndAsync();
+                    string errorOutput = await process.StandardError.ReadToEndAsync();
+                    //プロセス終了を待機
+                    process.WaitForExit();
+                    // プロセス終了を待機
+                    //await tcs.Task;
+
+                    // 出力を表示（必要ならUIやログに表示）
+                    Debug.Print("exe:" + output);
+                    Debug.Print("exe error" + errorOutput);
+
+                    return output;
+
+                    
+                }
+
+            }
+            catch (Exception e)
+            {
+                Debug.Print("exe error" + e);
+                return ""; // エラー時の戻り値
+            }
+        }
+    }
+}
